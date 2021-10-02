09bd730a938056feb483eb10839362d4ae2a234c
Konrád Kovács <konrad.kovacs97@outlook.com>
2021-10-02 16:13:27 +0200

    adding the simple sc

---
+++ simple_nft_sol/contracts/SimpleCollection.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollection is erc721 {
+    uint256 public tokenCounter;
+    constructor  () public ERC721("Dogtags","DGT"){
+    tokenCounter =0;
+    }
+
+function CreateCollection (string memory tokenURI) public  returns(uint256){
+
+    uint256 newItemID=tokenCounter;
+    _safeMint(msg.sender,newItemID);
+    _setTokenURI(newItemIDE,TokenURI);
+    tokencounter++;
+    return newitemID;
+}
+
+
+}
+
+
+
+
